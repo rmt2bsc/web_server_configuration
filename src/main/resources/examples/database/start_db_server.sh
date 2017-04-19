824fe8d64efa96f939bd169fb3926bc63837a093
roy terrell <rmt2bsc@gmail.com>
2017-04-19 14:32:41 -0500

    Added.

---
+++ src/main/resources/examples/database/start_db_server.sh
@@ -0,0 +1,4 @@
+#  change directory to where the database script lives, and execute the following command:  
+#    source start_db_server.sh
+
+/Applications/SQLAnywhere16/System/bin64s/dbsrv16 -n RMT2DALSYBSRV01 -ud /Users/royterrell/data/sybase/accounting.db /Users/royterrell/data/sybase/authentication.db /Users/royterrell/data/sybase/mime.db /Users/royterrell/data/sybase/projecttracker.db /Users/royterrell/data/sybase/rmt2.db /Users/royterrell/data/sybase/addressbook.db /Users/royterrell/data/sybase/framework.db
