028ff0b8865d656f4459ba9ed71bbfa5c79a7e47
appdev <royterrell2@gmail.com>
2018-09-07 14:06:00 -0500

    IS-35:  Added notes for createing various types of SQL Anywhere instances for Windows environment.   Revised MAC OS command line statement to create server as "rmt2devdb02"

--- src/main/resources/examples/database/start_db_server.sh
+++ src/main/resources/examples/database/start_db_server.sh
@@ -4,10 +4,14 @@
 ##############################################################
 ### Uncomment one of the following based on target platform
 ##############################################################
-# Windows - Should be implemented via Windows Service
+# Windows - Should be implemented as a Windows Service via Sybase Central or...
+# dbsvc -as -s auto -t network -w rmt2devdb02 "C:\Program Files\SQL Anywhere 16\Bin64\dbsrv16.exe" -x tcpip -c 512m /data/sybase/v16/accounting.db /data/sybase/v16/authentication.db /data/sybase/v16/contacts.db /data/sybase/v16/mime.db /data/sybase/v16/projecttracker.db /data/sybase/v16/rmt2.db
+# or as stand alone...
+# dbsrv16 -n rmt2devdb02 /data/sybase/v16/accounting.db /data/sybase/v16/authentication.db /data/sybase/v16/contacts.db /data/sybase/v16/mime.db /data/sybase/v16/projecttracker.db /data/sybase/v16/rmt2.db
+
 
 # MAC OS
-#/Applications/SQLAnywhere16/System/bin64s/dbsrv16 -n RMT2DALSYBSRV01 -ud /Users/royterrell/data/sybase/accounting.db /Users/royterrell/data/sybase/authentication.db /Users/royterrell/data/sybase/mime.db /Users/royterrell/data/sybase/projecttracker.db /Users/royterrell/data/sybase/rmt2.db /Users/royterrell/data/sybase/addressbook.db /Users/royterrell/data/sybase/framework.db
+#/Applications/SQLAnywhere16/System/bin64s/dbsrv16 -n rmt2devdb01 -ud /Users/royterrell/data/sybase/accounting.db /Users/royterrell/data/sybase/authentication.db /Users/royterrell/data/sybase/mime.db /Users/royterrell/data/sybase/projecttracker.db /Users/royterrell/data/sybase/rmt2.db /Users/royterrell/data/sybase/addressbook.db /Users/royterrell/data/sybase/framework.db
 
 # Ubuntu Production
 # /opt/sqlanywhere16/bin64/dbspawn -f dbsrv16 -n rmt2proddb01 /rmt2/data/accounting.db /rmt2/data/authentication.db /rmt2/data/contacts.db /rmt2/data/mime.db /rmt2/data/projecttracker.db /rmt2/data/rmt2.db
