c80a9a4787ee8410b78e916d632a3e3ce9ff405c
appdev <royterrell2@gmail.com>
2018-09-13 16:07:10 -0500

    IS-35: Added -o and -os switches to Capture The Server Console Log In Production and to rename and restart the db log when it grows too large, respectively.

--- src/main/resources/examples/database/Unix-Linux/start_db_server.sh
+++ src/main/resources/examples/database/Unix-Linux/start_db_server.sh
@@ -1,20 +1,2 @@
-#  change directory to where the database script lives, and execute the following command:  
-#    source start_db_server.sh
+/opt/sqlanywhere16/bin64/dbspawn -f dbsrv16 -n rmt2proddb01 -o /rmt2/logs/rmt2proddb01.log -os 10M /rmt2/data/accounting.db /rmt2/data/authentication.db /rmt2/data/contacts.db /rmt2/data/mime.db /rmt2/data/projecttracker.db /rmt2/data/rmt2.db
 
-##############################################################
-### Uncomment one of the following based on target platform
-##############################################################
-# Windows 
-# Can be implemented as a Windows Service via Sybase Central 
-# or create the windows service via command line for SQL Anywhere v16 or higher...
-# dbsvc -as -s auto -t network -w rmt2devdb02 -n rmt2devdb02 "C:\Program Files\SQL Anywhere 16\Bin64\dbsrv16.exe" -x tcpip -c 512m /data/sybase/v16/accounting.db /data/sybase/v16/authentication.db /data/sybase/v16/contacts.db /data/sybase/v16/mime.db /data/sybase/v16/projecttracker.db /data/sybase/v16/rmt2.db
-
-# or as stand alone...
-# dbsrv16 -n rmt2devdb02 /data/sybase/v16/accounting.db /data/sybase/v16/authentication.db /data/sybase/v16/contacts.db /data/sybase/v16/mime.db /data/sybase/v16/projecttracker.db /data/sybase/v16/rmt2.db
-
-
-# MAC OS
-#/Applications/SQLAnywhere16/System/bin64s/dbsrv16 -n rmt2devdb01 -ud /Users/royterrell/data/sybase/accounting.db /Users/royterrell/data/sybase/authentication.db /Users/royterrell/data/sybase/mime.db /Users/royterrell/data/sybase/projecttracker.db /Users/royterrell/data/sybase/rmt2.db /Users/royterrell/data/sybase/addressbook.db /Users/royterrell/data/sybase/framework.db
-
-# Ubuntu Production
-# /opt/sqlanywhere16/bin64/dbspawn -f dbsrv16 -n rmt2proddb01 /rmt2/data/accounting.db /rmt2/data/authentication.db /rmt2/data/contacts.db /rmt2/data/mime.db /rmt2/data/projecttracker.db /rmt2/data/rmt2.db
