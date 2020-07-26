5ffe6491b76bc9a5b0aea13fc5420c6d8c08b22d
appdev <royterrell2@gmail.com>
2020-07-26 13:56:40 -0500

    Changed file transport from FTP to SCP since the rmtdalmedia01 has been changed to a window server to a lInux server.   Added and/or refinded echo statments.

--- docs/database/Unix-Linux/backup_db.sh
+++ docs/database/Unix-Linux/backup_db.sh
@@ -1,5 +1,7 @@
 i#!/bin/bash
 
+echo ===== Start database backup...
+
 # Build file name
 days=$(date +%Y_%m_%d_%H%M)
 echo $days
@@ -7,35 +9,49 @@ file="archives_$days.tar.gz"
 echo $file
 loc="/rmt2/db_archives"
 
-# cpmpress all database files into one archive
-echo Archiving and compressing all files in $loc...
+# compress all database files into one archive
+echo ===== Archiving and compressing all files in $loc...
 tar cvzf $loc/$file $loc/*.*
-echo Compressed archiving is completed#echo Compressing $loc/$file...
+echo ===== Compressing $loc/$file is completed
 
 chmod ugo+rwx $loc/$file
 cd $loc
 
+################################################################
+# Now target machine is UNIX based, use SCP to copy file.
+# NOTE:  Used SSH private/public key to prevent having to 
+#        explicitly provide password for target machine when
+#        invoking the SCP command in batch file.
+################################################################
+echo ===== Copying DB archive...
+scp $file royterrell@rmtdalmedia01:~/Dropbox/MyData/db
+echo ===== DB archive copy completed.
+
+######################################################################################
+# Disabled FTP logic since we are no longer using a Windows machine as the target
+######################################################################################
 # ftp file to another server
-HOST=192.168.0.101   #rmtdalmedia01
-USER=admin02
-PASSWD=610hoover
-echo FTP $loc/$file to $HOST
+#HOST=192.168.0.5   #rmtdalmedia01
+#USER=admin02
+#PASSWD=610hoover
+#echo FTP $loc/$file to $HOST
 
-ftp -inv $HOST <<EOF
-user $USER $PASSWD
-prompt
-binary
-put $file
-quit
-EOF
+#ftp -inv $HOST <<EOF
+#user $USER $PASSWD
+#prompt
+#binary
+#put $file
+#quit
+#EOF
+#
+#if [ $? -ne 0 ]
+#   # Do error handleing here...
+#   exit 1
+#fi
 
-if [ $? -ne 0 ]
-   # Do error handleing here...
-   exit 1
-fi
-
-
+echo ===== Deleting DB archive from source...
 rm /rmt2/db_archives/*.*
+echo ===== Database backup completed!
 
 #  To untar:  tar xvzf <tar file> -C <destinatin path>
 
